3dbe34d8bcd8fc325c35e31809018893d109045b
cristiandev <cristianporretydev@gmail.com>
2022-11-14 11:51:34 -0500

    write a layout of the player loggin

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 import logic.*;
+import logic.functionalities.TableBuilder;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String args[]){
-        
+        TableBuilder
+                .VerifieTheWinner
+                .PlayerShape figure = TableBuilder
+                                        .VerifieTheWinner
+                                        .PlayerShape.valueOf(JOptionPane.showInputDialog("Chose your shape:"));
+        Player player = new Player("federico", figure);
     }
 
 }
