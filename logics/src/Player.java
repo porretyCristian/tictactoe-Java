6e3531aab7765de36cb2dc72838b37121653d566
cristiandev <cristianporretydev@gmail.com>
2022-11-14 10:20:03 -0500

    created the player classes

---
+++ logics/src/Player.java
@@ -0,0 +1,8 @@
+import functionalities.actionsPlayer;
+
+public class Player implements actionsPlayer{
+    @Override
+    public void moveFigure(int row, int column) {
+        
+    }
+}
---
+++ logics/src/SecondPlayer.java
@@ -0,0 +1,3 @@
+public class SecondPlayer extends Player{
+
+}
